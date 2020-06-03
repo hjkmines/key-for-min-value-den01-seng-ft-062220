87a00cbe3fb3dce8961259793bc54c8951cca717
Hyung Kim <hjkmines@gmail.com>
2020-06-03 06:03:11 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,9 +4,9 @@
 def key_for_min_value(name_hash)
 
   smallest_key = ""
+  smallest_value = 100000000000
   name_hash.each do |key, value|
     
-    smallest_value = value 
     if value < smallest_value 
       smallest_key = key
     elsif name_hash == {}
